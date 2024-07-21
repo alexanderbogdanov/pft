5c9332b64ea90e549315ae33801bdc4664d1fb86
alexandrbogdanov <al1029ex@gmail.com>
2024-07-21 17:57:59 +0200

    first test refactored

--- CreateGroup.py
+++ CreateGroup.py
@@ -11,19 +11,28 @@ class TestCreateGroup:
         chrome_options = Options()
         chrome_service = Service(ChromeDriverManager().install())
         self.driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
-        self.driver.implicitly_wait(30)
-        self.base_url = "https://www.google.com/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
+        # self.driver.implicitly_wait(30)
+        # self.base_url = "https://www.google.com/"
+        # self.verificationErrors = []
+        # self.accept_next_alert = True
         yield
         self.driver.quit()
 
     def test_create_group(self):
         wd = self.driver
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, "admin", "secret")
         self.open_groups_page(wd)
-        self.create_group(wd)
+        self.create_group(wd, "group2", "group header", "group footer")
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def test_create_empty_group(self):
+        wd = self.driver
+        self.open_home_page(wd)
+        self.login(wd, "admin", "secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, "", "", "")
         self.return_to_groups_page(wd)
         self.logout(wd)
 
@@ -33,29 +42,29 @@ class TestCreateGroup:
     def return_to_groups_page(self, wd):
         wd.find_element(By.LINK_TEXT, "group page").click()
 
-    def create_group(self, wd):
+    def create_group(self, wd, name, header, footer):
         wd.find_element(By.NAME, "new").click()
         wd.find_element(By.NAME, "group_name").click()
         wd.find_element(By.NAME, "group_name").clear()
-        wd.find_element(By.NAME, "group_name").send_keys("group2")
+        wd.find_element(By.NAME, "group_name").send_keys(name)
         wd.find_element(By.NAME, "group_header").click()
         wd.find_element(By.NAME, "group_header").clear()
-        wd.find_element(By.NAME, "group_header").send_keys("group header")
+        wd.find_element(By.NAME, "group_header").send_keys(header)
         wd.find_element(By.NAME, "group_footer").click()
         wd.find_element(By.NAME, "group_footer").clear()
-        wd.find_element(By.NAME, "group_footer").send_keys("group footer")
+        wd.find_element(By.NAME, "group_footer").send_keys(footer)
         wd.find_element(By.NAME, "submit").click()
 
     def open_groups_page(self, wd):
         wd.find_element(By.LINK_TEXT, "groups").click()
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element(By.NAME, "user").click()
         wd.find_element(By.NAME, "user").clear()
-        wd.find_element(By.NAME, "user").send_keys("admin")
+        wd.find_element(By.NAME, "user").send_keys(username)
         wd.find_element(By.NAME, "pass").click()
         wd.find_element(By.NAME, "pass").clear()
-        wd.find_element(By.NAME, "pass").send_keys("secret")
+        wd.find_element(By.NAME, "pass").send_keys(password)
         wd.find_element(By.XPATH, "//input[@value='Login']").click()
 
     def open_home_page(self, wd):
